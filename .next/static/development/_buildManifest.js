b908f6018db02e716de8f61517fc982e7372044b
Aniruth Ananthanarayanan <aniruth2211@gmail.com>
2023-06-27 20:47:42 -0500

    Delete node_modules and .next

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/tournaments/[tournamentId]":["static\u002Fchunks\u002Fpages\u002Ftournaments\u002F[tournamentId].js"],sortedPages:["\u002F_app","\u002F_error","\u002Ftournaments\u002F[tournamentId]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
